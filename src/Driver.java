af5f8565aeb57395614b9d905bf1ad77b2111cd2
Robert <rrdbn4@mst.edu>
2015-03-26 20:36:29 -0500

    added a frame because why not

--- src/Driver.java
+++ src/Driver.java
@@ -1,1 +1,35 @@
-// The most amazing code in all the land goes here
+package code;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+Robert Dunn, Holly Busken, Matt Lindner
+*/
+public class Driver extends JFrame
+{
+
+  public Driver() 
+  {
+    super("Ricart and Agrawala");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(800,600); 
+    setVisible(true);
+    
+    validate();
+    repaint();
+  } 
+
+  public static void main(String[] args)
+  {
+    new Driver();
+
+  }
+
+  public void paint(Graphics g)
+  {
+    super.paint(g);
+    g.drawOval(100, 100, 100, 100);
+  }
+}
