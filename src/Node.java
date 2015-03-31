fce04bd769ee1002408584ac5a7a8141deac90a5
hmbwbc <hmbwbc@mst.edu>
2015-03-30 21:19:03 -0500

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -8,14 +8,15 @@ public class Node
   String color;
   int x;
   int y;
+  //ever state corresponds to a direction
   int state;
-  public Node(int id, String clr, int xcord, int ycord)
+  public Node(int id, String clr, int xcord, int ycord, int st)
   {
     ID=id;
-	  color=clr;
-	  x=xcord;
-	  y=ycord;
-	  state=0;
+	color=clr;
+	x=xcord;
+	y=ycord;
+	state=st;
   }
  
 }
