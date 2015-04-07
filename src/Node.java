08c5627d1d19f7b8717c6e7c8b61dc18fb5ea3c6
rrdbn4 <rrdbn4@mst.edu>
2015-04-07 15:23:48 -0500

    relocated updateNode() to be a member function

--- src/Node.java
+++ src/Node.java
@@ -19,26 +19,216 @@ public class Node
     state=st;
   }
   
-  public String getDirection(Direction state)
+  // public String getDirection(Direction state)
+  // {
+  //   String direction ="Unknown";
+  //   if(state == Direction.NORTH)
+  //     direction = "North";
+  //   else if(state == Direction.SOUTH_EAST)
+  //     direction = "South East";
+  //   else if(state == Direction.EAST)
+  //     direction = "East";
+  //   else if(state == Direction.NORTH_EAST)
+  //     direction = "North East";
+  //   else if(state == Direction.SOUTH)
+  //     direction = "South";
+  //   else if(state == Direction.NORTH_WEST)
+  //     direction = "North West";
+  //   else if(state == Direction.WEST)
+  //     direction = "West";
+  //   else if(state == Direction.SOUTH_WEST)
+  //     direction = "South West";
+  //   return direction;
+  // }
+
+  public void update(View inst)
   {
-    String direction ="Unknown";
+    int updown=15;
+    int diagonal=20;
     if(state == Direction.NORTH)
-      direction = "North";
-    else if(state == Direction.SOUTH_EAST)
-      direction = "South East";
-    else if(state == Direction.EAST)
-      direction = "East";
-    else if(state == Direction.NORTH_EAST)
-      direction = "North East";
-    else if(state == Direction.SOUTH)
-      direction = "South";
-    else if(state == Direction.NORTH_WEST)
-      direction = "North West";
-    else if(state == Direction.WEST)
-      direction = "West";
-    else if(state == Direction.SOUTH_WEST)
-      direction = "South West";
-    return direction;
+    {
+      //Going North on track
+      if(x==inst.x1 && y==inst.y1)
+      {      
+        state = Direction.SOUTH_EAST;
+        //slope = y2-y1 / x2-x1
+        int xdiff=inst.x2-inst.x1;
+        int ydiff=inst.y2-inst.y1;
+
+        x=x+(xdiff/diagonal);
+        y=y+(ydiff/diagonal);
+      }
+      else
+      {
+        y=y-updown;
+        if(y<inst.y1)
+        {
+          x=inst.x1;
+          y=inst.y1;
+          state=Direction.SOUTH_EAST;
+        }
+      }
+    }
+  else if(state==Direction.SOUTH_EAST)
+  {
+    //Going South East on track
+    if(x==inst.x2 && y==inst.y2)
+    {
+      //Entering the bridge going East
+      state=Direction.EAST;
+      x=x+updown;
+    }
+    else
+    {
+      int xdiff=inst.x2-inst.x1;
+      int ydiff=inst.y2-inst.y1;    
+      x=x+(xdiff/diagonal);
+      y=y+(ydiff/diagonal);  
+      if(x>inst.x2 || y>inst.y2)
+      {
+        x=inst.x2;
+        y=inst.y2;
+        state=Direction.EAST;
+      } 
+    }
+  }
+  else if(state==Direction.EAST)
+  {
+    //Moving East on the bridge 
+    if(x==inst.x3 && y==inst.y2)
+    {
+      //Leaving the bridge to go North East
+      state=Direction.NORTH_EAST;
+      int xdiff=inst.x4-inst.x3;
+      int ydiff=inst.y1-inst.y2;    
+      x=x+(xdiff/diagonal);
+      y=y+(ydiff/diagonal);   
+    }
+    else
+    {
+      x=x+updown;
+      if(x>inst.x3)
+      {
+        x=inst.x3;
+        y=inst.y2;
+        state=Direction.NORTH_EAST;
+      }
+    }
+  }
+  else if(state==Direction.NORTH_EAST)
+  {
+    //Moving North East on track
+    if(x==inst.x4 && y==inst.y1)
+    {
+      state=Direction.SOUTH;
+    y=y+updown;
+    }
+    else
+    {
+      int xdiff=inst.x4-inst.x3;
+      int ydiff=inst.y1-inst.y2;    
+      x=x+(xdiff/diagonal);
+      y=y+(ydiff/diagonal); 
+      if(x>inst.x4 || y<inst.y1)
+      {
+      x=inst.x4;
+      y=inst.y1;
+      state=Direction.SOUTH;
+      } 
+    }
+  }
+  else if(state==Direction.SOUTH)
+  {
+    //Moving South on track
+    if(x==inst.x4 && y==inst.y3)
+    {
+      state=Direction.NORTH_WEST;
+      int xdiff=inst.x3-inst.x4;
+      int ydiff=inst.y2-inst.y3;    
+      x=x+(xdiff/diagonal);
+      y=y+(ydiff/diagonal);    
+    }
+    else
+    {
+      y=y+updown;
+    if(y>inst.y3)
+    {
+      x=inst.x4;
+    y=inst.y3;
+    state=Direction.NORTH_WEST;
+    }
+    }
+  }
+  else if(state==Direction.NORTH_WEST)
+  {
+    //Moving North West on track
+    if(x==inst.x3 && y==inst.y2)
+    {
+      //Now entering the bridge to go West
+      state=Direction.WEST;
+      x=x-updown;
+    }
+    else
+    {
+    int xdiff=inst.x3-inst.x4;
+    int ydiff=inst.y2-inst.y3;    
+    x=x+(xdiff/diagonal);
+    y=y+(ydiff/diagonal);
+    if(x<inst.x3 || y<inst.y2)
+    {
+    x=inst.x3;
+    y=inst.y2;
+    state=Direction.WEST;
+    } 
+    }
+  }
+  else if(state==Direction.WEST)
+  {
+    //Moving West on the Bridge
+    if(x==inst.x2 && y==inst.y2)
+    {
+      //Leaving Bridge to go South West
+      state=Direction.SOUTH_WEST;
+    int xdiff=inst.x1-inst.x2;
+    int ydiff=inst.y3-inst.y2;    
+    x=x+(xdiff/diagonal);
+    y=y+(ydiff/diagonal);     
+    }
+    else
+    {
+      x=x-updown;
+    if(x<inst.x2)
+    {
+      x=inst.x2;
+    y=inst.y2;
+    state=Direction.SOUTH_WEST;
+    }
+    }
+  }
+  else if(state==Direction.SOUTH_WEST)
+  {
+    //Moving South West on track
+    if(x==inst.x1 && y==inst.y3)
+    {
+      state=Direction.NORTH;
+      y=y-updown;
+    }
+    else
+    {
+      int xdiff=inst.x1-inst.x2;
+      int ydiff=inst.y3-inst.y2;    
+      x=x+(xdiff/diagonal);
+      y=y+(ydiff/diagonal); 
+      if(x<inst.x1 || y > inst.y3)
+    {
+      x=inst.x1;
+    y=inst.y3;
+    state=Direction.NORTH;
+    }
+    }
+    
+  }
+  
   }
  
 }
--- src/View.java
+++ src/View.java
@@ -43,20 +43,11 @@ public class View extends JPanel implements ActionListener
     super.paintComponent(g);
     if(firstTime)
     {
-	  /*
-      x1 = getInsets().left + (int)(getWidth() * 0.15);
-      x2 = getInsets().left + (int)(getWidth() * 0.35);
-      x3 = getInsets().left + (int)(getWidth() * 0.65);
-      x4 = getInsets().left + (int)(getWidth() * 0.85);
-      y1 = getInsets().top + (int)(getHeight() * 0.2);
-      y2 = getInsets().top + (int)(getHeight() * 0.5);
-      y3 = getInsets().top + (int)(getHeight() * 0.8);
-      */
-	  
-	  final int height=600;
-	  final int width=800;
-	  
-	  x1 = getInsets().left + (int)(width * 0.15);
+      
+      final int height=600;
+      final int width=800;
+      
+      x1 = getInsets().left + (int)(width * 0.15);
       x2 = getInsets().left + (int)(width * 0.35);
       x3 = getInsets().left + (int)(width * 0.65);
       x4 = getInsets().left + (int)(width * 0.85);
@@ -90,10 +81,10 @@ public class View extends JPanel implements ActionListener
     g.drawLine(x3, y2, x4, y3);
   
     //Update nodes
-    updateNode(redA);
-    updateNode(redB);
-    updateNode(blueA);
-    updateNode(blueB);  
+    redA.update(this);
+    redB.update(this);
+    blueA.update(this);
+    blueB.update(this);  
   
     //draw Nodes
     int diameter=10;
@@ -104,195 +95,5 @@ public class View extends JPanel implements ActionListener
     g.setColor(Color.blue);
     g.fillOval(blueA.x-rad,blueA.y-rad,diameter,diameter);
     g.fillOval(blueB.x-rad,blueB.y-rad,diameter,diameter);
-  }
-  
-  public void updateNode(Node node)
-  {
-    int updown=15;
-    int diagonal=20;
-    if(node.state == Direction.NORTH)
-    {
-      //Going North on track
-      if(node.x==x1 && node.y==y1)
-      {      
-        node.state = Direction.SOUTH_EAST;
-        //slope = y2-y1 / x2-x1
-        int xdiff=x2-x1;
-        int ydiff=y2-y1;
-
-        node.x=node.x+(xdiff/diagonal);
-        node.y=node.y+(ydiff/diagonal);
-      }
-      else
-      {
-        node.y=node.y-updown;
-        if(node.y<y1)
-        {
-    	    node.x=x1;
-          node.y=y1;
-          node.state=Direction.SOUTH_EAST;
-        }
-      }
-    }
-  else if(node.state==Direction.SOUTH_EAST)
-  {
-    //Going South East on track
-    if(node.x==x2 && node.y==y2)
-    {
-      //Entering the bridge going East
-      node.state=Direction.EAST;
-      node.x=node.x+updown;
-    }
-    else
-    {
-      int xdiff=x2-x1;
-      int ydiff=y2-y1;    
-      node.x=node.x+(xdiff/diagonal);
-      node.y=node.y+(ydiff/diagonal);  
-      if(node.x>x2 || node.y>y2)
-      {
-        node.x=x2;
-        node.y=y2;
-        node.state=Direction.EAST;
-      }	
-    }
-  }
-  else if(node.state==Direction.EAST)
-  {
-    //Moving East on the bridge 
-    if(node.x==x3 && node.y==y2)
-    {
-      //Leaving the bridge to go North East
-      node.state=Direction.NORTH_EAST;
-      int xdiff=x4-x3;
-      int ydiff=y1-y2;    
-      node.x=node.x+(xdiff/diagonal);
-      node.y=node.y+(ydiff/diagonal);   
-    }
-    else
-    {
-      node.x=node.x+updown;
-      if(node.x>x3)
-      {
-        node.x=x3;
-        node.y=y2;
-        node.state=Direction.NORTH_EAST;
-      }
-    }
-  }
-  else if(node.state==Direction.NORTH_EAST)
-  {
-    //Moving North East on track
-    if(node.x==x4 && node.y==y1)
-    {
-      node.state=Direction.SOUTH;
-    node.y=node.y+updown;
-    }
-    else
-    {
-      int xdiff=x4-x3;
-      int ydiff=y1-y2;    
-      node.x=node.x+(xdiff/diagonal);
-      node.y=node.y+(ydiff/diagonal); 
-      if(node.x>x4 || node.y<y1)
-      {
-	    node.x=x4;
-	    node.y=y1;
-	    node.state=Direction.SOUTH;
-      }	
-    }
-  }
-  else if(node.state==Direction.SOUTH)
-  {
-    //Moving South on track
-    if(node.x==x4 && node.y==y3)
-    {
-      node.state=Direction.NORTH_WEST;
-      int xdiff=x3-x4;
-      int ydiff=y2-y3;    
-      node.x=node.x+(xdiff/diagonal);
-      node.y=node.y+(ydiff/diagonal);    
-    }
-    else
-    {
-      node.y=node.y+updown;
-	  if(node.y>y3)
-	  {
-	    node.x=x4;
-		node.y=y3;
-		node.state=Direction.NORTH_WEST;
-	  }
-    }
-  }
-  else if(node.state==Direction.NORTH_WEST)
-  {
-    //Moving North West on track
-    if(node.x==x3 && node.y==y2)
-    {
-      //Now entering the bridge to go West
-      node.state=Direction.WEST;
-      node.x=node.x-updown;
-    }
-    else
-    {
-    int xdiff=x3-x4;
-    int ydiff=y2-y3;    
-    node.x=node.x+(xdiff/diagonal);
-    node.y=node.y+(ydiff/diagonal);
-    if(node.x<x3 || node.y<y2)
-    {
-	  node.x=x3;
-	  node.y=y2;
-	  node.state=Direction.WEST;
-    }	
-    }
-  }
-  else if(node.state==Direction.WEST)
-  {
-    //Moving West on the Bridge
-    if(node.x==x2 && node.y==y2)
-    {
-      //Leaving Bridge to go South West
-      node.state=Direction.SOUTH_WEST;
-    int xdiff=x1-x2;
-    int ydiff=y3-y2;    
-    node.x=node.x+(xdiff/diagonal);
-    node.y=node.y+(ydiff/diagonal);     
-    }
-    else
-    {
-      node.x=node.x-updown;
-	  if(node.x<x2)
-	  {
-	    node.x=x2;
-		node.y=y2;
-		node.state=Direction.SOUTH_WEST;
-	  }
-    }
-  }
-  else if(node.state==Direction.SOUTH_WEST)
-  {
-    //Moving South West on track
-    if(node.x==x1 && node.y==y3)
-    {
-      node.state=Direction.NORTH;
-      node.y=node.y-updown;
-    }
-    else
-    {
-      int xdiff=x1-x2;
-      int ydiff=y3-y2;    
-      node.x=node.x+(xdiff/diagonal);
-      node.y=node.y+(ydiff/diagonal); 
-      if(node.x<x1 || node.y > y3)
-	  {
-	    node.x=x1;
-		node.y=y3;
-		node.state=Direction.NORTH;
-	  }
-    }
-    
-  }
-  
   }
 }
