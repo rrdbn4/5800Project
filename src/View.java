56230be2822b66b0a60475c8bace7eb7f48f3b69
mdlkwd <mdlkwd@mst.edu>
2015-05-11 14:25:30 -0500

    More View.java comments

--- src/View.java
+++ src/View.java
@@ -36,10 +36,13 @@ public class View extends JPanel implements ChangeListener, ActionListener
   
   public View()
   {
+		// Defines the size of the Java window
     setPreferredSize(new Dimension(width, height)); 
+		// Makes this JPanel not invisible
     setVisible(true);
     validate();
 
+		// Set the initial position of the intersection points
     x1 = getInsets().left + (int)(width * 0.15);
     x2 = getInsets().left + (int)(width * 0.35);
     x3 = getInsets().left + (int)(width * 0.65);
@@ -56,7 +59,7 @@ public class View extends JPanel implements ChangeListener, ActionListener
     nodes[1] = new Node(1,"red",x1,y3-num,Direction.NORTH, this, 200);
     nodes[2] = new Node(2,"blue",x4,y1+num,Direction.SOUTH, this, 250);
     nodes[3] = new Node(3,"blue",x4,y3-num,Direction.SOUTH, this, 250); 
-    
+		
     speedSliders = new JSlider[numNodes];
     sliderLabels = new JLabel[numNodes];
     for (int sliderID = 0; sliderID < numNodes; sliderID++)
@@ -64,10 +67,14 @@ public class View extends JPanel implements ChangeListener, ActionListener
     	JPanel movementContainer = new JPanel();
     	movementContainer.setLayout(new GridLayout(2, 1));
     	
+			// Creates a label to indicate which slider belongs to which 
+			// Node, and adds it to the container.
     	sliderLabels[sliderID] = new JLabel("Person " + sliderID, SwingConstants.CENTER);
     	sliderLabels[sliderID].setForeground(((sliderID < 2)? Color.red : Color.blue));
     	movementContainer.add(sliderLabels[sliderID]);
     	
+			// Creates a slider that controls the speed of a node, 
+			// and adds it to the container.
     	speedSliders[sliderID] = new JSlider(10, 230, 200);
     	speedSliders[sliderID].setPreferredSize(new Dimension(120, 50));
     	speedSliders[sliderID].addChangeListener(this);
@@ -76,6 +83,7 @@ public class View extends JPanel implements ChangeListener, ActionListener
     	add(movementContainer);
     }
     
+		// Adds the JCheckbox too the container.
     allowMultiple = new JCheckBox("Allow Multiple People 1-Way", false);
     allowMultiple.addActionListener(this);
     add(allowMultiple);
@@ -85,12 +93,12 @@ public class View extends JPanel implements ChangeListener, ActionListener
   {
     super.paintComponent(g);
 
-    //left triangle
+    // Draws the left triangle
     g.drawLine(x1, y1, x1, y3);
     g.drawLine(x1, y1, x2, y2);
     g.drawLine(x1, y3, x2, y2);
 
-    //bridge
+    // Draws the bridge connecting the two triangles.
     g.drawLine(x2, y2, x3, y2);
 
     //right triangle
@@ -110,8 +118,10 @@ public class View extends JPanel implements ChangeListener, ActionListener
       g.fillOval(nodes[i].x-rad,nodes[i].y-rad,diameter,diameter);
   }
 
+	// Event listener. When one of the speed sliders is adjusted,
+	// this function changes the "speed" of the appropriate node.
   public void stateChanged(ChangeEvent e)
-  { System.out.println("here");
+  {
 	if (e.getSource() == speedSliders[0])
 	{
 		nodes[0].setSpeed(speedSliders[0].getValue());
@@ -130,6 +140,9 @@ public class View extends JPanel implements ChangeListener, ActionListener
 	}
   }
 
+	// Event listener.  When the JCheckbox is selected, this 
+	// tells the nodes whether or not they are allowed to cross the bridge
+	// in the same direction at the same time.
   public void actionPerformed(ActionEvent e)
   {
 	for (int i = 0; i < numNodes; i++)
